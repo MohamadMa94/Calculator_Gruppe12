dc3eacb7b23a9e5849c677b80aecceae4adceacf
maheralky <maherjood94@gmail.com>
2022-09-12 23:49:38 +0200

    Add project files.

---
+++ Calculator/Calculator1.cs
@@ -0,0 +1,31 @@
+﻿
+
+namespace Calculator
+{
+    public class Calculator1
+    {
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double Power(double x, double exp)
+        {
+            double result;
+            result = Math.Pow(x, exp);
+
+            return result;
+        }
+
+    }
+}
---
+++ CalculatorTest/CalculatorTest.cs
@@ -0,0 +1,70 @@
+using Calculator;
+
+namespace CalculatorTest
+{
+    public class CalculatorTest
+
+
+    {
+
+        private Calculator1 Calculator;
+        [SetUp]
+        public void Setup()
+        {
+
+            Calculator  = new Calculator1();
+
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+
+        [TestCase(4, 5, 9)]
+        [TestCase(4, 0, 4)]
+         public void Add_ReturnsTheSum(double a, double b, double result)
+        {
+            
+
+            double sum = Calculator.Add(a, b);
+            Assert.That(sum, Is.EqualTo(result));
+
+        }
+
+        [TestCase(4, 4, 0)]
+        [TestCase(9, 1, 8)]
+
+        public void Subtract_ReturnsTheSum(double a, double b, double result)
+        {           
+
+            double sum = Calculator.Subtract(a, b);
+
+            Assert.That(sum, Is.EqualTo(result).Within(0.01));
+
+        }
+        [TestCase(6, 4, 24)]
+        [TestCase(36, 0, 0)]
+        public void Subtract_1ReturnsTheSum(double a, double b, double result)
+        {
+            
+            double sum = Calculator.Multiply(a, b);
+            Assert.That(sum, Is.EqualTo(result));
+
+        }
+
+        [TestCase(4, 4, 256)]
+        [TestCase(4, 0, 1)]
+        public void SubtractPowerReturnsTheSum(double x, double exp, double result)
+        {
+         
+            double sum = Calculator.Power(x, exp);
+
+            Assert.That(sum, Is.EqualTo(result).Within(0.1));
+
+        }
+
+    }
+}
